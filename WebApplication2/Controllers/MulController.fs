19f975954e649cc9e03f79b7362a8cab59f1ca17
FinnT730 <FinnT730@gmail.com>
2021-12-13 15:49:58 +0100

    Added multiply

---
+++ WebApplication2/Controllers/MulController.fs
@@ -0,0 +1,29 @@
+﻿namespace WebApplication2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open WebApplication2
+
+[<ApiController>]
+[<Route("[controller]")>]
+type MulController (logger : ILogger<MulController>) =
+    inherit ControllerBase()
+//    
+//    [<HttpPost("num")>]
+//    member _.finn() =
+//        0
+
+
+    [<HttpOptions>]
+    [<HttpGet>]
+    member _.Get([<HttpPost>]num: int) =
+        { _val = (num * num) }
+//        [|
+//            for index in 0..100 ->
+//                { isEven = (index % 2 = 0)
+//                  index = index }
+//        |]
---
+++ WebApplication2/Mul.fs
@@ -0,0 +1,8 @@
+namespace WebApplication2
+
+open System
+
+type Mul =
+    { _val: int}
+//    member this._index =
+//        this.index
