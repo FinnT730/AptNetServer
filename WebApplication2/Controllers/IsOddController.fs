301228aa44078b23c408ceea0f974a024a7aac7b
FinnT730 <FinnT730@gmail.com>
2021-12-13 15:33:14 +0100

    isOdd.fs added!

---
+++ WebApplication2/Controllers/IsOddController.fs
@@ -0,0 +1,30 @@
+﻿namespace WebApplication2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open WebApplication2
+
+[<ApiController>]
+[<Route("[controller]")>]
+type IsOddController (logger : ILogger<IsOddController>) =
+    inherit ControllerBase()
+//    
+//    [<HttpPost("num")>]
+//    member _.finn() =
+//        0
+
+
+    [<HttpOptions>]
+    [<HttpGet>]
+    member _.Get([<HttpPost>]num: double) =
+        { isOdd = (num % 2.0 = 1.0)
+          index = num }
+//        [|
+//            for index in 0..100 ->
+//                { isEven = (index % 2 = 0)
+//                  index = index }
+//        |]
---
+++ WebApplication2/isOdd.fs
@@ -0,0 +1,9 @@
+namespace WebApplication2
+
+open System
+
+type isOdd =
+    { isOdd: bool
+      index: double }
+//    member this._index =
+//        this.index
