33be9b4d1b341ae380f0fd495d820f31af468a90
FinnT730 <FinnT730@gmail.com>
2021-12-13 15:26:59 +0100

    Init

---
+++ WebApplication2/Controllers/isEvenController.fs
@@ -0,0 +1,30 @@
+﻿namespace WebApplication2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open WebApplication2
+
+[<ApiController>]
+[<Route("[controller]")>]
+type isEvenController (logger : ILogger<isEvenController>) =
+    inherit ControllerBase()
+//    
+//    [<HttpPost("num")>]
+//    member _.finn() =
+//        0
+
+
+    [<HttpOptions>]
+    [<HttpGet>]
+    member _.Get([<HttpPost>]num: double) =
+        { isEven = (num % 2.0 = 0.0)
+          index = num }
+//        [|
+//            for index in 0..100 ->
+//                { isEven = (index % 2 = 0)
+//                  index = index }
+//        |]
---
+++ WebApplication2/isEven.fs
@@ -0,0 +1,9 @@
+namespace WebApplication2
+
+open System
+
+type isEven =
+    { isEven: bool
+      index: double }
+//    member this._index =
+//        this.index
